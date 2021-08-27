723dee67bae1fd4f8df847eedbc50c60bf5de4f7
vincent <to@toto.fr>
2021-08-27 17:03:55 +0200

    Ajout d'un lien sur les buttons du formulaire d'inscription

--- front-end/src/components/IdentificationForm/Button/Button.js
+++ front-end/src/components/IdentificationForm/Button/Button.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import "./Button.scss"
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Button.scss";
 
-const Button = ({name}) => {
-    return (
-        <button>
-            {name}
-        </button>
-    );
+const Button = ({ name }) => {
+  return (
+    <NavLink exact to="/">
+      <button>{name}</button>
+    </NavLink>
+  );
 };
 
 export default Button;
