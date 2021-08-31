d955ef9348a8e1685e9def22221d5f1a33e13129
vincent <to@toto.fr>
2021-08-31 14:22:47 +0200

    Revert "Avec une key c'est tout de suite plus facile ! Bug du texte persistant entre les inputs corrigé"
    This reverts commit 8d8231fb9428d97c89f0c48eada66c9aa71d7843.

--- front-end/src/components/IdentificationForm/Form/Form.js
+++ front-end/src/components/IdentificationForm/Form/Form.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { v4 as uuidv4 } from 'uuid';
 import Button from "../Button/Button";
 import Input from "../../UI/Input/Input";
 import "./Form.scss"
@@ -10,17 +9,17 @@ const Form = ({ form }) => {
       <form className="form">
           {form === "register" ? 
           <>
-          <Input className="input_container" key={uuidv4()} type="text" placeholder="Prénom" id="firstname" name="firstname" min="2" max="40" />
-          <Input className="input_container" key={uuidv4()} type="text" placeholder="Nom" id="lastname" name="lastname" min="2" max="60" />
-          <Input className="input_container" key={uuidv4()} type="email" placeholder="Votre adresse mail" id="email" name="email" />
-          <Input className="input_container" key={uuidv4()} type="password" placeholder="Mot de passe" id="password" name="password" min="10" max="32" />
-          <Input className="input_container" key={uuidv4()} type="password" placeholder="Confirmez le mot de passe" id="verify-password" name="verify-password" min="10" max="32" />
+          <Input className="input_container" type="text" placeholder="Prénom" id="firstname" name="firstname" min="2" max="40" />
+          <Input className="input_container" type="text" placeholder="Nom" id="lastname" name="lastname" min="2" max="60" />
+          <Input className="input_container" type="email" placeholder="Votre adresse mail" id="email" name="email" />
+          <Input className="input_container" type="password" placeholder="Mot de passe" id="password" name="password" min="10" max="32" />
+          <Input className="input_container" type="password" placeholder="Confirmez le mot de passe" id="verify-password" name="verify-password" min="10" max="32" />
           <Button name="Inscription" />
         </>
         :
         <>
-        <Input className="input_container" key={uuidv4()} type="email" placeholder="Adresse mail" id="email" name="email" min="2" max="40" />
-        <Input className="input_container" key={uuidv4()} type="password" placeholder="Mot de passe" id="password" name="password" min="10" max="32" />
+        <Input className="input_container" type="email" placeholder="Adresse mail" id="email" name="email" min="2" max="40" />
+        <Input className="input_container" type="password" placeholder="Mot de passe" id="password" name="password" min="10" max="32" />
         <Button name="Connexion" />
         </>
         }
