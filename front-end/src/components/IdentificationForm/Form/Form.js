caafe24e35ee8b00f533bfe9ccb05324a97eff8b
vincent <to@toto.fr>
2021-08-31 21:28:11 +0200

    ajout d'une key sur les input du formulaire de connexion

--- front-end/src/components/IdentificationForm/Form/Form.js
+++ front-end/src/components/IdentificationForm/Form/Form.js
@@ -3,23 +3,25 @@ import Button from "../Button/Button";
 import Input from "../../UI/Input/Input";
 import "./Form.scss"
 
+import { v4 as uuidv4 } from 'uuid';
+
 const Form = ({ form }) => {
 
     return (
       <form className="form">
           {form === "register" ? 
           <>
-          <Input className="input_container" type="text" placeholder="Prénom" id="firstname" name="firstname" min="2" max="40" />
-          <Input className="input_container" type="text" placeholder="Nom" id="lastname" name="lastname" min="2" max="60" />
-          <Input className="input_container" type="email" placeholder="Votre adresse mail" id="email" name="email" />
-          <Input className="input_container" type="password" placeholder="Mot de passe" id="password" name="password" min="10" max="32" />
-          <Input className="input_container" type="password" placeholder="Confirmez le mot de passe" id="verify-password" name="verify-password" min="10" max="32" />
+          <Input key={uuidv4()} className="input_container" type="text" placeholder="Prénom" id="firstname" name="firstname" min="2" max="40" />
+          <Input key={uuidv4()} className="input_container" type="text" placeholder="Nom" id="lastname" name="lastname" min="2" max="60" />
+          <Input key={uuidv4()} className="input_container" type="email" placeholder="Votre adresse mail" id="email" name="email" />
+          <Input key={uuidv4()} className="input_container" type="password" placeholder="Mot de passe" id="password" name="password" min="10" max="32" />
+          <Input key={uuidv4()} className="input_container" type="password" placeholder="Confirmez le mot de passe" id="verify-password" name="verify-password" min="10" max="32" />
           <Button name="Inscription" />
         </>
         :
         <>
-        <Input className="input_container" type="email" placeholder="Adresse mail" id="email" name="email" min="2" max="40" />
-        <Input className="input_container" type="password" placeholder="Mot de passe" id="password" name="password" min="10" max="32" />
+        <Input key={uuidv4()} className="input_container" type="email" placeholder="Adresse mail" id="email" name="email" min="2" max="40" />
+        <Input key={uuidv4()} className="input_container" type="password" placeholder="Mot de passe" id="password" name="password" min="10" max="32" />
         <Button name="Connexion" />
         </>
         }
