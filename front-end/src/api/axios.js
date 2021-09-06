06b0e8784331325bfa7224f68e8fcdcb17e207ed
vincent <to@toto.fr>
2021-09-06 20:27:58 +0200

    Creation des fichiers relatif à la configuration d'axios, mais sans l'utiliser pour le moment

---
+++ front-end/src/api/axios.js
@@ -0,0 +1,34 @@
+const Axios = require('axios');
+
+Axios.defaults.baseURL = 'https://react-http-8bdda-default-rtdb.europe-west1.firebasedatabase.app';
+Axios.defaults.headers.post['Content-Type'] = 'application/json';
+Axios.defaults.timeout = 60000;
+
+const setRequestConfig = (queryParams) => {
+    const source = Axios.CancelToken.source();
+    let config = {
+        cancelToken: source.token,
+        params: {}
+    }
+    if (queryParams) {
+        config.params = queryParams;
+    }
+
+    return config;
+}
+
+export const GET = async (url, queryParams = null) => {
+    return await Axios.get(url, {...setRequestConfig(queryParams)})
+}
+
+export const POST = async (url, data = null, queryParams = null) => {
+    return Axios.post(url, data, {...setRequestConfig(queryParams)})
+}
+
+export const DELETE = async (url, queryParams = null) => {
+    return await Axios.delete(url, {...setRequestConfig(queryParams)})
+}
+
+export const PUT = async (url, data = null, queryParams = null) => {
+    return Axios.put(url, data, {...setRequestConfig(queryParams)})
+}
---
+++ front-end/src/api/endpoints.js
@@ -0,0 +1,6 @@
+const endpoints = {
+  GET_POSTS: "/api/post",
+  CREATE_POST: "/api/post",
+};
+
+export default endpoints;
