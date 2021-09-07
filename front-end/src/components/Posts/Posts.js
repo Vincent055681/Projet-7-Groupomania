0902c0ab1dc97b3b787344863d340470d6c75750
vincent <to@toto.fr>
2021-09-07 16:44:12 +0200

    utilisation des id renvoyé par le back plutot que des id de uuid

--- front-end/src/components/Posts/Posts.js
+++ front-end/src/components/Posts/Posts.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
 import Post from "./Post/Post";
 import "./Posts";
 import "./Posts.scss";
@@ -13,13 +12,14 @@ const Posts = () => {
     const toFetch = async () => {
       const axiosCall = await GET(ENDPOINTS.GET_POSTS);
       setDataApi(axiosCall.data);
+      console.log(axiosCall.data);
     };
     toFetch();
   }, []);
   return (
     <div className="posts">
       {dataApi.map((post) => {
-        return <Post post={post} key={uuidv4()} />;
+        return <Post post={post} key={post.id} />;
       })}
     </div>
   );
