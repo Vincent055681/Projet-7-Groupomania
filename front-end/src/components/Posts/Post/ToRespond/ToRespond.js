3fbe1a51cff79fbf75495a0fcd0e8ee4fae30132
vincent <to@toto.fr>
2021-10-13 18:50:57 +0200

    amélioration design input

--- front-end/src/components/Posts/Post/ToRespond/ToRespond.js
+++ front-end/src/components/Posts/Post/ToRespond/ToRespond.js
@@ -1,41 +1,47 @@
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState} from "react";
+import React, { useState } from "react";
 import "./ToRespond.scss";
 
 import { POST, PATCH } from "../../../../api/axios";
 import ENDPOINTS from "../../../../api/endpoints";
 
 const ToRespond = () => {
-
-
   // States
-  const [commentMessage, setCommentMessage] = useState('')
+  const [commentMessage, setCommentMessage] = useState("");
 
   const submitHandle = async (e) => {
-    e.preventDefault()
-    
+    e.preventDefault();
+
     const data = {
       author_id: JSON.parse(localStorage.getItem("user")).user_id,
       author_firstname: JSON.parse(localStorage.getItem("user")).user_firstname,
       author_lastname: JSON.parse(localStorage.getItem("user")).user_lastname,
       post_id: 144,
-      message: commentMessage
-    }
+      message: commentMessage,
+    };
     const response = await POST(ENDPOINTS.CREATE_COMMENT, data);
     console.log(response);
-
-  }
+  };
 
   const inputHandle = (e) => {
-    setCommentMessage(e.target.value)
-  }
+    setCommentMessage(e.target.value);
+  };
 
   return (
-    <form onSubmit={submitHandle}>
-      <input type="text" placeholder="Écrivez un commentaire..." onChange={inputHandle} value={commentMessage}/>
-      {/* <FontAwesomeIcon icon={faPaperPlane} /> */}
-    </form>
+    <>
+      <hr className="divider" />
+      <form onSubmit={submitHandle}>
+        <input
+          type="text"
+          placeholder="Écrivez un commentaire..."
+          onChange={inputHandle}
+          value={commentMessage}
+          id="input-comment"
+        />
+        {/* <FontAwesomeIcon icon={faPaperPlane} /> */}
+      </form>
+    </>
   );
 };
 
