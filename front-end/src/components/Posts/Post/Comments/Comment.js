4284f7d4f19fd07822dd97be17230c696bf6c4c1
vincent <to@toto.fr>
2021-10-13 18:40:42 +0200

    amélioration disposition name dans commentaire

--- front-end/src/components/Posts/Post/Comments/Comment.js
+++ front-end/src/components/Posts/Post/Comments/Comment.js
@@ -7,19 +7,22 @@ const relativeTime = require("dayjs/plugin/relativeTime");
 dayjs.extend(relativeTime);
 
 const Comment = ({ comment }) => {
-
   console.log(comment);
-  
+
   return (
     <div className="comment">
-     <div className="comment__author-id">{comment.author_id}</div>
-     <div className="comment__author-firstname">{comment.author_firstname}</div>
-     <div className="comment__author-lastname">{comment.author_lastname}</div>
-     <div className="comment__message">{comment.message}</div>
-     <div className="comment__date">{dayjs(comment.created_at).locale("fr").fromNow()}</div>
+      <div className="comment__author-infos">
+        <div className="comment__author-id">{comment.author_id}</div>
+        <div className="comment__author-name">
+          {`${comment.author_firstname} ${comment.author_lastname}`}
+        </div>
+      </div>
+      <div className="comment__message">{comment.message}</div>
+      <div className="comment__date">
+        {dayjs(comment.created_at).locale("fr").fromNow()}
+      </div>
     </div>
   );
 };
 
 export default Comment;
-
