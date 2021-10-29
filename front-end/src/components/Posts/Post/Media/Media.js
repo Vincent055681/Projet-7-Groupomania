9bb40e5ca9460cd9accda7155fcecf71deb51fa1
vincent <to@toto.fr>
2021-10-30 00:02:18 +0200

    Impossible d'afficher une image

--- front-end/src/components/Posts/Post/Media/Media.js
+++ front-end/src/components/Posts/Post/Media/Media.js
@@ -1,25 +1,13 @@
-import React, { useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import "./Media.scss";
 
 const Media = ({ mediaURL }) => {
   console.log(mediaURL);
-//   useEffect(() => {
-//     const toFetch = async () => {
-//       try {
-//         const response = await axios.get(media);
-//       } catch (err) {
-//         throw err;
-//       }
-//     };
-//     toFetch();
-//   }, []);
-
   return (
     <div className="media">
-      <img src={mediaURL} alt="MEDIA" class="media__img" />
+      {<img src={mediaURL} alt="myPicture" className="media__img" />}
     </div>
   );
-};
+}
 
 export default Media;
