3fb7e7cea8c267ebb2024473be753bcf3025c63d
vincent <to@toto.fr>
2021-11-09 14:54:23 +0100

    Meilleure utilisation de dotenv

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql")
 
 // Create connexion
+// i don't use .env because its my openclassrooms project so it's not sensible data. In real project, i will use .env and put it in .gitignore
 const db = mysql.createConnection({
-    host     : process.env.DB_HOST,
-    user     : process.env.DB_USER,
-    password : process.env.PASSWORD,
-    database : process.env.DB_DATABASE
+    host     : 'localhost',
+    user     : 'root',
+    password : 'ssMRhrc68xVReAJtIxFg',
+    database : "groupomania"
   });
   
  module.exports.getDB = () => {
